594c8e9b0c45445e843f40ba5e4f1b476502bc27
MacRandom <macrandomstudio@gmail.com>
2021-01-04 18:10:04 +0700

    Добавлен класс для проверки букв (гласная или согласная).

---
+++ TestTask/LetterAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace TestTask
+{
+    public static class LetterAnalyzer
+    {
+        private const string vowels = "аеёиоуыэюя";
+        private const string consonants = "бвгджзйклмнпрстфхцчшщъь";
+
+        public static bool IsVowel(char c) 
+            => vowels.Contains(c);
+        
+        public static bool IsConsonant(char c) 
+            => consonants.Contains(c);
+    }
+}
